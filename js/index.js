f1ab10ec8030956c28b49dfa2e6e0fdcd0386a7a
Eid-elatar <81574643+Eid-Hassan@users.noreply.github.com>
2022-04-05 18:48:35 +0200

    btn Scroll

--- js/index.js
+++ js/index.js
@@ -11,3 +11,19 @@ setInterval(() => {
     // Change Background Image Url
     landingPage.style.backgroundImage = 'url("../img/' + imgsArray[randomNumber] + '")';
 }, 10000 );
+
+
+let span = document.querySelector( ".up" );
+
+window.onscroll = function () {
+    // console.log( this.scrollY );
+    this.scrollY >= 1000 ? span.classList.add( "show" ) : span.classList.remove( "show" );
+}
+
+span.onclick = function ()
+{
+    window.scrollTo( {
+        top: 0,
+        behavior: "smooth",
+    } );
+}
